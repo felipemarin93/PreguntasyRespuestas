e24a6ca852ba81c147dc204ea7ca435071d5ea96
felipemarin93 <fmarin93@misena.edu.co>
2022-01-06 07:51:43 -0500

    Añadiendo Métodos y Favicon

--- clases/Jugador.js
+++ clases/Jugador.js
@@ -1,3 +1,5 @@
+/*
 export var nombreJugador = prompt("Antes de Iniciar, Cuentame cual es tu nombre");
 export var puntajeJugador=0;
 export var dineroJugador=0;
+*/
---
+++ clases/Metodos.js
@@ -0,0 +1,37 @@
+// @ts-check
+
+import {Pregunta} from './Pregunta.js';
+
+export class Metodos {
+
+    indicePregunta = 0;
+    puntajeJugador = 0;
+
+    /**
+     * 
+     * @param {Pregunta[]} preguntas 
+     */
+
+    constructor(preguntas) {
+        this.preguntas = preguntas;
+    }
+
+    /**
+     * 
+     * @returns {Pregunta} Retorna la pregunta encontrada
+     */
+    obtenerIndicePregunta() {
+        return this.preguntas[this.indicePregunta]
+    }
+    /**
+     * 
+     * @param {string} respuesta respuesta escogida por el jugador
+     */
+    advinar(respuesta) {
+        if(this.obtenerIndicePregunta().respuestaCorrecta(respuesta)){
+            this.puntajeJugador++;
+        }
+        this.indicePregunta++;
+    }
+}
+
